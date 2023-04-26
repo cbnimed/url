869b26fa05b9e693a93bf78cc0468cfde6c05bd7
cbnimed <130749004+cbnimed@users.noreply.github.com>
2023-04-26 13:32:23 +0700

    Add files via upload

---
+++ urlhtml.js
@@ -0,0 +1,12 @@
+//<![CDATA[
+var uri = window.location.toString();
+if (uri.indexOf("&.html","&.html") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("&.html"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(".html",".html") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(".html"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+//]]>
